f0d5c1868c68092c30ca4eb39c98e8eb7a211616
hype-R <ttan360@aucklanduni.ac.nz>
2017-12-19 17:09:47 +1300

    Added drag and drop

---
+++ OOPVR/Assets/_Scripts/ObjectController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+    public Transform Holder;  //Holder for holding the object
+    public Rigidbody objct; //Object to be picked up and dropped
+
+    public GameObject AllVariables;
+    private Vector3 objScale;
+
+    void Start()
+    {
+        objScale = objct.transform.localScale;
+    }
+
+    public void InHands()
+    {
+        objct.transform.parent = Holder.transform;
+        objct.transform.position = Holder.transform.position;
+
+        AllVariables.SetActive(true);
+    }
+
+    public void OnTheShelf()
+    {
+        //objct.transform.parent = null;
+        objct.transform.parent = Holder.transform;
+        objct.transform.localPosition = Holder.transform.localPosition;
+        objct.transform.rotation = Holder.transform.rotation;
+
+        objct.transform.localScale = objScale;
+
+        AllVariables.SetActive(false);
+    }
+}
---
+++ OOPVR/Assets/_Scripts/RadialGazeTimer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialGazeTimer : MonoBehaviour {
+
+
+    
+    public float NumberOfSecondsForSelection = 3f;
+    public Transform RadialProgress;
+    public string place;
+
+    public GameObject ToDoAction;
+    private float counterTimer = 0;
+
+    // Use this for initialization
+    void Start () {
+
+        RadialProgress.GetComponent<Image>().fillAmount = 0;
+
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+        
+        counterTimer += Time.deltaTime;
+        RadialProgress.GetComponent<Image>().fillAmount = counterTimer/NumberOfSecondsForSelection;
+
+        if (counterTimer >= NumberOfSecondsForSelection)
+        {
+            PerformActionOnGameObject();
+
+        }
+        
+    }
+
+    public void ResetCounter()
+    {
+        counterTimer = 0f;
+        RadialProgress.GetComponent<Image>().fillAmount = counterTimer;
+    }
+
+
+    public void PerformActionOnGameObject()
+    {
+        if(place == "HAND")
+        {
+            ResetCounter();
+            ToDoAction.GetComponent<ObjectController>().InHands();
+        }
+        else if (place == "SHELF")
+        {
+            ResetCounter();
+            ToDoAction.GetComponent<ObjectController>().OnTheShelf();
+        }
+
+        
+    }
+}
