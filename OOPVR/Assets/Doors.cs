89bc6b9bbd2cf0698215842f0e333111ad9d0334
RSGWA <r.akauola@gmail.com>
2017-12-20 13:41:35 +1300

    Added Doors

---
+++ OOPVR/Assets/Doors.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour {
+
+	Animator animator;
+	bool doorOpen;
+
+	void Start() {
+		doorOpen = false;
+		animator = GetComponent<Animator>();
+	}
+
+	void DoorControl(string direction) {
+		animator.SetTrigger(direction);
+	}
+
+	public void ControlDoor() {
+		if (doorOpen == false) {
+			DoorControl ("Open");
+			doorOpen = true;
+		} else {
+			DoorControl("Close");
+			doorOpen = false;
+		}
+	}
+}
