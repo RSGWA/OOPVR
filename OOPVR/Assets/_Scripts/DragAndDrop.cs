1e60944efbc74beb9c2030d12ec7724203db9436
RSGWA <r.akauola@gmail.com>
2018-01-03 17:40:42 +1300

    Drag and Drop animation [Work in Progress]

--- OOPVR/Assets/_Scripts/DragAndDrop.cs
+++ OOPVR/Assets/_Scripts/DragAndDrop.cs
@@ -10,24 +10,31 @@ public class DragAndDrop : MonoBehaviour {
 	private static GameObject objInHand;
 	private static bool isObjInHand;
 
+	AnimationCurve xCurve;
+	AnimationCurve yCurve;
+	AnimationCurve zCurve;
+	Keyframe[] ks;
+
+	//bool move;
+
 	void Start()
 	{
 		Hand = GameObject.Find ("Hand");
 		boxes = GameObject.FindGameObjectsWithTag ("Box");
+		setUpAnimation ();
+		//move = false;
 		enableBoxes (false);
 		isObjInHand = false;
 	}
 
-	void Update() {
-		if (isObjInHand) {
-			// Object follow reticle
-		}
-	}
-
 	public void InHands()
 	{
-		transform.parent = Hand.transform.parent;
-		transform.position = Hand.transform.position;
+		//transform.parent = Hand.transform.parent;
+		//transform.position = Hand.transform.position;
+		moveToHand();
+		Debug.Log (transform.position.x);
+		Debug.Log (transform.position.y);
+		Debug.Log (transform.position.z);
 		objInHand = this.gameObject;
 		isObjInHand = true;
 		enableBoxes (true);
@@ -38,7 +45,6 @@ public class DragAndDrop : MonoBehaviour {
 		objInHand.transform.parent = transform.parent;
 		objInHand.transform.position = transform.position;
 		objInHand.transform.rotation = transform.rotation;
-		objInHand.transform.localScale = transform.localScale;
 		
 		enableBoxes (false);
 		isObjInHand = false;
@@ -49,6 +55,36 @@ public class DragAndDrop : MonoBehaviour {
 		foreach (GameObject box in boxes) {
 			box.GetComponent<BoxCollider> ().enabled = enable;
 		}
+	}
+
+	void setUpAnimation() 
+	{
+		ks = new Keyframe[2];
+
+		Keyframe idle = new Keyframe (0,transform.position.x);
+		Keyframe atHand = new Keyframe (100, Hand.transform.position.x);
+		ks [0] = idle;
+		ks [1] = atHand;
+
+		xCurve = new AnimationCurve (ks);
+
+		idle = new Keyframe (0, transform.position.y);
+		atHand = new Keyframe (100, Hand.transform.position.y);
+		ks [0] = idle;
+		ks [1] = atHand;
+
+		yCurve = new AnimationCurve (ks);
+
+		idle = new Keyframe (0, transform.position.z);
+		atHand = new Keyframe (100, Hand.transform.position.z);
+		ks [0] = idle;
+		ks [1] = atHand;
+
+		zCurve = new AnimationCurve (ks);
+	}
+
+	void moveToHand() {
+		transform.position = new Vector3 (xCurve.Evaluate (Time.time), yCurve.Evaluate (Time.time), zCurve.Evaluate (Time.time));
 	}
 
 }
---
+++ OOPVR/Assets/_Scripts/Variable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour {
+	private AnimationCurve anim;
+	private Keyframe[] ks;
+
+	public GameObject Hand;
+
+	void Start() {
+		ks = new Keyframe[50];
+		Keyframe beginning = new Keyframe (0, transform.position.x);
+		int i = 0;
+		while (i < ks.Length) {
+			ks [i] = new Keyframe (i, i * i);
+			i++;
+		}
+		anim = new AnimationCurve (ks);
+	}
+
+	void Update() {
+		transform.position = new Vector3 (Time.time, anim.Evaluate (Time.time), 0);
+	}
+}
