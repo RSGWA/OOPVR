261a6a4c9a4af879bb7303b8e95cb4cd87c0799d
hype-R <ttan360@aucklanduni.ac.nz>
2018-01-04 15:47:47 +1300

    Added Parameter Platform

---
+++ OOPVR/Assets/_Scripts/PlatformScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    private Animator _anim;
+    GameObject _go = null;
+    bool active = false;
+
+	// Use this for initialization
+	void Start () {
+
+        _anim = GetComponent<Animator>();
+        _go = GameObject.FindGameObjectWithTag("Door");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Control()
+    {
+        if (!active)
+        {
+            _anim.SetTrigger("Activate");
+            /*if (_go != null)
+            {
+                _go.GetComponent<Doors>().ControlDoor();
+            }*/
+            active = true;
+        }
+        else
+        {
+            _anim.SetTrigger("Deactivate");
+            active = false;
+        }
+
+    }
+
+}
