a95348a816ad15f0692062d1978511d333e18ee8
hype-R <ttan360@aucklanduni.ac.nz>
2018-01-26 05:09:36 +1300

    Added info fuctionality (WIP)
    -added info icons
    -infocanvas to show information
    -infoController for writing text to canvas.

---
+++ OOPVR/Assets/InfoController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InfoController : MonoBehaviour {
+
+    private TextMeshProUGUI infoStatus;
+    private GameObject canvas;
+
+    // Use this for initialization
+    void Start()
+    {
+        infoStatus = GameObject.Find("InfoText").GetComponent<TextMeshProUGUI>();
+        canvas = GameObject.Find("InfoCanvas");
+        canvas.GetComponent<CanvasGroup>().alpha = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetInformation(string msg)
+    {
+        infoStatus.text = msg;
+       // StartCoroutine(UpdateMessage());
+    }
+
+    private IEnumerator UpdateMessage()
+    {
+        ShowInformation();
+        yield return new WaitForSeconds(4);
+        HideInformation();
+    }
+
+    public void ShowInformation()
+    {
+        canvas.GetComponent<CanvasGroup>().alpha = 1;
+    }
+
+    public void HideInformation()
+    {
+        canvas.GetComponent<CanvasGroup>().alpha = 0;
+    }
+}
