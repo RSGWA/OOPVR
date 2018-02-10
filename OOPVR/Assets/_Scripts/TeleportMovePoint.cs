5156005975b9e8d08a5c85ff5e0b75f89cb2c746
hype-R <ttan360@aucklanduni.ac.nz>
2018-02-10 14:56:54 +1300

    Made new addition to the move point
    - added script to control behaviour
    - updated prefab
    - added glowing effect from DaydreamElements

---
+++ OOPVR/Assets/_Scripts/TeleportMovePoint.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportMovePoint : MonoBehaviour {
+
+    GameObject Player;
+    GameObject glow;
+
+
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.Find("Player");
+        glow = transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Player.transform.position.x == transform.position.x)
+        {
+            DisableMovePoint();
+        }
+        else
+        {
+            EnableMovePoint();
+        }
+	}
+
+   
+    void EnableMovePoint()
+    {
+        print(transform.TransformPoint(transform.position) + "    world space");
+        print(transform.position + "     position");
+        //disable selection
+        transform.GetComponent<Collider>().enabled = true;
+
+        //disable glow
+        glow.SetActive(true);
+
+    }
+
+    
+    void DisableMovePoint()
+    {
+        transform.GetComponent<Collider>().enabled = false; //disable selection
+        glow.SetActive(false); //disable glow
+    }
+
+    
+    public void ShowMovePoint()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    //hide the movepoints - when instantiating an object
+    public void HideMovePoint()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
