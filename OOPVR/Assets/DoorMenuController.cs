cf581f227ee47c95ee95e15cb6f8d81b5114b006
hype-R <ttan360@aucklanduni.ac.nz>
2018-02-15 19:17:08 +1300

    Added Door Menu Controller for the activities
    - it is attached to the ActivityController gameObject

--- OOPVR/Assets/DoorMenuController.cs
+++ OOPVR/Assets/DoorMenuController.cs
@@ -1,16 +1,96 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class DoorMenuController : MonoBehaviour {
+public class DoorMenuController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private string ActivityName;
+
+    private List<GameObject> inactiveDoors = new List<GameObject>();
+
+    Transform activeDoorOptionMenuPanel;
+    GameObject GoIntoButton, InfoButton, activeGoInto, activeInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        GetActiveDoor(scene.name);
+        GetAllDoors();
+
+        foreach (GameObject method in inactiveDoors)
+        {
+            Transform doorExtOptionPanel = method.transform.GetChild(0).Find("OptionMenu").Find("Panel");
+            GoIntoButton = doorExtOptionPanel.Find("GoIntoButton").gameObject;
+            InfoButton = doorExtOptionPanel.Find("InfoButton").gameObject;
+
+            ShowButton(GoIntoButton, false);
+            ShowButton(InfoButton, true);
+
+            GameObject doorIntDoorPanel = method.transform.GetChild(1).Find("DoorPanel").gameObject;
+            SetInteractive(doorIntDoorPanel, false);
+        }
+
+
+        GameObject activityMethod = GameObject.Find(ActivityName);
+        activeDoorOptionMenuPanel = activityMethod.transform.Find("Door").GetChild(0).Find("OptionMenu").Find("Panel");
+        activeGoInto = activeDoorOptionMenuPanel.Find("GoIntoButton").gameObject;
+        activeInfo = activeDoorOptionMenuPanel.Find("InfoButton").gameObject;
+
+        ShowButton(activeGoInto, true);
+        ShowButton(activeInfo, true);
+
+        GameObject activeIntDoorPanel = activityMethod.transform.Find("Door").transform.GetChild(1).Find("DoorPanel").gameObject;
+        SetInteractive(activeIntDoorPanel, false);
+    }
+
+    void GetActiveDoor(string activityName)
+    {
+        switch (activityName)
+        {
+            case "DefaultConstructorActivity":
+                ActivityName = "DefaultConstructor";
+                break;
+            case "2ParameterConstructor":
+                ActivityName = "Constructor";
+                break;
+            case "SetNameActivity":
+                ActivityName = "SetName";
+                break;
+            case "GetName":
+                ActivityName = "GetName";
+                break;
+            case "IncrementAgeActivity":
+                ActivityName = "IncrementAge";
+                break;
+            default:
+                break;
+        }
+    }
+    void GetAllDoors()
+    {
+        GameObject[] allDoors = GameObject.FindGameObjectsWithTag("Door");
+
+        foreach (GameObject doors in allDoors)
+        {
+            if (doors.name == "Door")
+            {
+                inactiveDoors.Add(doors);
+            }
+        }
+    }
+
+    void SetInteractive(GameObject doorPanel, bool key)
+    {
+        doorPanel.GetComponent<InteractiveItemGaze>().enabled = key;
+    }
+
+    void ShowButton(GameObject button, bool key)
+    {
+        button.SetActive(key);
+    }
 }
