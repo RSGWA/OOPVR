dbd35f7de04ecadf497f9360b501003fe3c2efa4
RSGWA <r.akauola@gmail.com>
2018-02-16 17:19:50 +1300

    Reworked Main Menu with play buttons working - code button is a WIP

---
+++ OOPVR/Assets/_Scripts/Activity.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Activity : MonoBehaviour {
+
+	private string activityTitle;
+
+	// Use this for initialization
+	void Awake () {
+		activityTitle = this.name;
+	}
+
+	void Start() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void play() {
+		switch (activityTitle) {
+		case "ConstructorNoParameters":
+			SceneManager.LoadScene ("DefaultConstructorScene", LoadSceneMode.Single);
+			break;
+		case "ConstructorWithParameters":
+			SceneManager.LoadScene ("2ParameterConstructor", LoadSceneMode.Single);
+			break;
+		case "SetName":
+			SceneManager.LoadScene ("SetNameActivity", LoadSceneMode.Single);
+			break;
+		case "GetName":
+			SceneManager.LoadScene ("GetName", LoadSceneMode.Single);
+			break;
+		case "IncrementAge":
+			SceneManager.LoadScene ("IncrementAgeActivity", LoadSceneMode.Single);
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void showCodeOrDescription() {
+
+	}
+}
