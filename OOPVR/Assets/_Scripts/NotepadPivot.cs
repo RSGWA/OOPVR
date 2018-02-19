85ff0147cf6e7fddf88cd1b514b0bb4ceda2dc0c
RSGWA <r.akauola@gmail.com>
2018-02-19 13:22:03 +1300

    Transition of Notepad moving made smoother

--- OOPVR/Assets/_Scripts/NotepadPivot.cs
+++ OOPVR/Assets/_Scripts/NotepadPivot.cs
@@ -14,38 +14,44 @@ public class NotepadPivot : MonoBehaviour {
 	void Update () {
 		transform.rotation = Quaternion.Euler (-30, Camera.main.transform.rotation.eulerAngles.y, 0);
 
-
 		if (Camera.main.transform.rotation.eulerAngles.y > -10 && Camera.main.transform.rotation.eulerAngles.y < 10) {
-			transform.localPosition = new Vector3 (0, transform.localPosition.y, 0.5f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (0, transform.localPosition.y, 0.5f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (0, transform.localPosition.y, 0.5f);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 35 && Camera.main.transform.rotation.eulerAngles.y < 55) {
-			transform.localPosition = new Vector3 (0.35f, transform.localPosition.y, 0.35f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (0.35f, transform.localPosition.y, 0.35f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (0.35f, transform.localPosition.y, 0.35f);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 80 && Camera.main.transform.rotation.eulerAngles.y < 100) {
-			transform.localPosition = new Vector3 (0.5f, transform.localPosition.y, 0);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (0.5f, transform.localPosition.y, 0), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (0.5f, transform.localPosition.y, 0);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 125 && Camera.main.transform.rotation.eulerAngles.y < 145) {
-			transform.localPosition = new Vector3 (0.35f, transform.localPosition.y, -0.35f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (0.35f, transform.localPosition.y, -0.35f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (0.35f, transform.localPosition.y, -0.35f);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 160 && Camera.main.transform.rotation.eulerAngles.y < 190) {
-			transform.localPosition = new Vector3 (0, transform.localPosition.y, -0.5f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (0, transform.localPosition.y, -0.5f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (0, transform.localPosition.y, -0.5f);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 215 && Camera.main.transform.rotation.eulerAngles.y < 235) {
-			transform.localPosition = new Vector3 (-0.35f, transform.localPosition.y, -0.35f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (-0.35f, transform.localPosition.y, -0.35f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (-0.35f, transform.localPosition.y, -0.35f);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 260 && Camera.main.transform.rotation.eulerAngles.y < 280) {
-			transform.localPosition = new Vector3 (-0.5f, transform.localPosition.y, 0);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (-0.5f, transform.localPosition.y, 0), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (-0.5f, transform.localPosition.y, 0);
 		}
 
 		if (Camera.main.transform.rotation.eulerAngles.y > 305 && Camera.main.transform.rotation.eulerAngles.y < 325) {
-			transform.localPosition = new Vector3 (-0.35f, transform.localPosition.y, 0.35f);
+			transform.localPosition = Vector3.MoveTowards (transform.localPosition, new Vector3 (-0.35f, transform.localPosition.y, 0.35f), 2 * Time.deltaTime);
+			//transform.localPosition = new Vector3 (-0.35f, transform.localPosition.y, 0.35f);
 		}
-
 	}
 }
