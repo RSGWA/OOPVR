fbcff88c9f3a62fddb81f00ee623bff56cc650f6
hype-R <ttan360@aucklanduni.ac.nz>
2018-06-26 13:00:37 +1200

    blueprint able to instantiate two instances

--- OOPVR/Assets/_Scripts/BlueprintController.cs
+++ OOPVR/Assets/_Scripts/BlueprintController.cs
@@ -4,31 +4,35 @@ using System.Timers;
 using System.ComponentModel;
 using UnityEngine;
 
-public class BlueprintController : MonoBehaviour {
+public class BlueprintController : MonoBehaviour
+{
 
-	private OptionMenu optionsMenu;
-	public GameObject instance;
+    private OptionMenu optionsMenu;
+    //public GameObject instance;
+    public GameObject[] instances;
 
-	private bool menuOpen;
+    private bool menuOpen;
 
-	private Vector3 originalSize;
-	private Vector3 highlightedSize;
-	private Vector3 growth = new Vector3 (0.015f, 1, 0.015f);
+    private Vector3 originalSize;
+    private Vector3 highlightedSize;
+    private Vector3 growth = new Vector3(0.015f, 1, 0.015f);
 
-	private Animator anim;
+    private Animator anim;
     private InfoController info;
     private Status MessageCanvas;
     private bool bpInPosition = false;
     bool infoSelected = false;
     bool alreadyInstantiated = false;
+    private int count = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         optionsMenu = transform.GetComponent<OptionMenu>();
-		
-		menuOpen = false;
-		anim = GetComponent<Animator> ();
+
+        menuOpen = false;
+        anim = GetComponent<Animator>();
         info = GameObject.Find("InfoCanvas").GetComponent<InfoController>();
         MessageCanvas = GameObject.Find("MessageCanvas").GetComponent<Status>();
     }
@@ -44,47 +48,66 @@ public class BlueprintController : MonoBehaviour {
             optionsMenu.Deselect();
             moveBlueprint();
         }
-        
+
     }
 
-	public void moveBlueprint() {
-		
-		// Disable collider so gaze timer doesnt activate
-		GetComponent<BoxCollider>().enabled = false;
+    public void moveBlueprint()
+    {
 
-		BPControl ("Move");
-		StartCoroutine ("check");
-		StartCoroutine ("createInstance");
-	}
+        // Disable collider so gaze timer doesnt activate
+        GetComponent<BoxCollider>().enabled = false;
 
-	// Checks whether blueprint is in position for instance animations to begin
-	IEnumerator check() {
-		bpInPosition = false;
+        BPControl("Move");
+        StartCoroutine("check");
+        StartCoroutine("createInstance");
+        //BPControl("Return");
+    }
 
-		while (!bpInPosition) {
-			yield return null;
+    // Checks whether blueprint is in position for instance animations to begin
+    IEnumerator check()
+    {
+        bpInPosition = false;
 
-			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("BPInPosition")) {
-				bpInPosition = true;
-			}		
-		}
-	}
+        while (!bpInPosition)
+        {
+            yield return null;
 
-	IEnumerator createInstance() {
-		while (!bpInPosition) {
-			yield return new WaitForSeconds (0.1f);
-		}
-		instance.GetComponent<InstanceController> ().createInstance ();
-	}
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("BPInPosition"))
+            {
+                bpInPosition = true;
+            }
+        }
+    }
 
-	void BPControl(string direction) {
-		anim.SetTrigger(direction);
-	}
+    IEnumerator createInstance()
+    {
+        while (!bpInPosition)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
 
-	public void returnToOrigin() {
-		this.gameObject.SetActive (false);
-		BPControl ("Return");
-	}
+        if (count == (instances.Length))
+        {
+            MessageCanvas.SetMessage("CANNOT PERFORM ACTION : This Activity only allows you to instantiate " + instances.Length + " instance(s) from the blueprint");
+        }
+        else
+        {
+            instances[count].GetComponent<InstanceController>().createInstance();
+            count++;
+        }
+
+    }
+
+    void BPControl(string direction)
+    {
+        anim.SetTrigger(direction);
+    }
+
+    public void returnToOrigin()
+    {
+        BPControl("Return");
+        GetComponent<BoxCollider>().enabled = true;
+    }
 
     public void InfoButton()
     {
